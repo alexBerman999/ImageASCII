e5da5644de3cc8a86479491918b967d60b0c2c28
Alex Berman <inser7name@gmail.com>
2017-10-14 16:48:55 -0400

    Made images wider to counteract distortion

--- ImageProcess.py
+++ ImageProcess.py
@@ -2,18 +2,18 @@ import numpy
 from PIL import Image
 
 charScale = "\".:-=+*#%@" #List of characters from light to dark by apparant shading
-scale = 0.5 #Scale to modify image size by
+scale = 0.25 #Scale to modify image size by
 
 def imgToAscii(img):
   #Reduce image size
-  scaleSize = img.size[0] * scale, img.size[1] * scale 
-  img.thumbnail(scaleSize, Image.ANTIALIAS)
+  scaleSize = (int(img.size[0] * 2 * scale), int(img.size[1] * scale)) #Double width as characters are taller than they are wide
+  imgMod = img.resize(scaleSize, Image.ANTIALIAS)
   #Convert to greyscale
-  imgBW = img.convert('L')
+  imgMod = imgMod.convert('L')
   
   #Get data of pixels
-  pixels = list(imgBW.getdata(0)) #Gets value of only red pixels as, in a greyscale image, r = g = b
-  width, height = imgBW.size
+  pixels = list(imgMod.getdata(0)) #Gets value of only red pixels as, in a greyscale image, r = g = b
+  width, height = imgMod.size
   pixels = [pixels[i * width:(i + 1) * width] for i in range(height)] #reformating into a 2D array
   
 #Storing corresponding ascii arrangement in an array to be returned
