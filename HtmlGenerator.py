d009862bac115e3245ba824eee45d505865c5254
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-10-15 12:12:54 +0000

    made test css

--- HtmlGenerator.py
+++ HtmlGenerator.py
@@ -8,7 +8,7 @@ ascii = AsciiImageProcess.imgToAscii(loc)
 #Open new file. TODO: Change name of file to make unique
 f = open("test.html", "w")
 #Consistent stuff
-f.write("<html>\n\t<head>\n\t\t<title></title>\n\t\t<meta content=\"\">\n\t\t<link rel=\"stylesheet\" href=\"css/style.css\">\n\t</head>\n\t<body>\n\t\t<h1>\n\t\t\tASCII Converter\n\t\t</h1>")
+f.write("<html>\n\t<head>\n\t\t<title>ASCII Art</title>\n\t\t<meta content=\"\">\n\t\t<link rel=\"stylesheet\" href=\"css/test.css\">\n\t</head>\n\t<body>\n\t\t<h1>\n\t\t\tASCII Converter\n\t\t</h1>")
 f.write(AsciiImageProcess.asciiArrayToHtml(ascii))
 f.write("\n\t\t<div id=\"img\">\n\t\t\t<img style = \"width:80%;\" src = " + loc + " alt=\"Image\">\n\t\t</div>\n\t</body>\n</html>")
 f.close()
