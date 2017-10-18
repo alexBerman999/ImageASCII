f7971b5c362f9dcf0eb736d9ac76e549801ee2de
Alex Berman <inser7name@gmail.com>
2017-10-17 23:18:13 -0400

    Removed debugging statements

--- HtmlGenerator.py
+++ HtmlGenerator.py
@@ -5,13 +5,4 @@ import AsciiImageProcess
 loc = sys.argv[1]
 #Convert image to ascii art
 ascii = AsciiImageProcess.imgToAscii(loc)
-#Open new file. TODO: Change name of file to make unique
-#f = open("test.html", "w")
-#Consistent stuff
-#f.write("<html>\n\t<head>\n\t\t<title>ASCII Art</title>\n\t\t<meta content=\"\">\n\t\t<link rel=\"stylesheet\" href=\"css/style.css\">\n\t</head>\n\t<body>\n\t\t<h1>\n\t\t\tASCII Converter\n\t\t</h1>")
-#f.write(AsciiImageProcess.asciiArrayToHtml(ascii))
-print("<h1>" + loc + "</h1>")
 print(AsciiImageProcess.asciiArrayToHtml(ascii))
-#f.write("\n\t\t<div id=\"img\">\n\t\t\t<img src = " + loc + " alt=\"Image\">\n\t\t</div>\n\t</body>\n</html>")
-#f.close()
-#print("/test.html")
