5863a525ee2c5d0b30cb3af3ec0f06053ec77f1f
Alex Berman <inser7name@gmail.com>
2017-10-17 22:44:41 -0400

    Actually executes command

--- convert.php
+++ convert.php
@@ -10,6 +10,7 @@
     <?php
       move_uploaded_file($_FILES["fileSelection"]["tmp_name"], $_SERVER['DOCUMENT_ROOT'] . 'uploads/' . $_FILES["fileSelection"]["name"]);
       $command = escapeshellcmd("python3 HtmlGenerator.py "$_SERVER['DOCUMENT_ROOT'] . 'uploads/' . $_FILES["fileSelection"]["name"]);
+      shell_exec($command);
       exit;
     ?>
   </body>
