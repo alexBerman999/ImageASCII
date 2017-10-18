c5f1e5fc5b3064f81180e44b55bde9306474ea53
Alex Berman <alexberman999@gmail.com>
2017-10-17 21:04:28 -0400

    Added print for status of image file move for testing

--- upload.php
+++ upload.php
@@ -2,5 +2,5 @@
 $tmp_name = $_FILES["fileSelection"]["tmp_name"];
 $name = basename($_FILES["fileSelection"]["name"]);
 echo $name;
-move_uploaded_file($tmp_name, $name)
+echo move_uploaded_file($tmp_name, $name)
 ?>
