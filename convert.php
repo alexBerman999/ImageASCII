d8b82f1f511cd377a37a5969757c2443ea318a36
Jason Carrete <jasoncarrete5@gmail.com>
2017-10-23 10:10:34 -0400

    Fixed convert.php formatting and syntax errors

--- convert.php
+++ convert.php
@@ -6,26 +6,24 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <h1>
-			ASCII Converter
-		</h1>
+    <h1>ASCII Converter</h1>
     <div class="header">
-      &nbsp;</br></br></br>      
+      &nbsp;<br><br><br>
     </div>
     <?php
         $target_file = basename($_FILES["fileSelection"]["name"]);
         $command = escapeshellcmd("python3 HtmlGenerator.py ".$_FILES["fileSelection"]["tmp_name"]);
         echo shell_exec($command);
-      //move_uploaded_file($_FILES["fileSelection"]["tmp_name"], $_SERVER['DOCUMENT_ROOT'] . 'uploads/' . $_FILES["fileSelection"]["name"]);
-      //$command = escapeshellcmd("python3 HtmlGenerator.py "."test.jpg");//$_FILES["fileSelection"]["tmp_name"]);//$_SERVER['DOCUMENT_ROOT'] . 'uploads/' . $_FILES["fileSelection"]["name"]);
-      //shell_exec($command);
-      //exit;
+        //move_uploaded_file($_FILES["fileSelection"]["tmp_name"], $_SERVER['DOCUMENT_ROOT'] . 'uploads/' . $_FILES["fileSelection"]["name"]);
+        //$command = escapeshellcmd("python3 HtmlGenerator.py "."test.jpg");//$_FILES["fileSelection"]["tmp_name"]);//$_SERVER['DOCUMENT_ROOT'] . 'uploads/' . $_FILES["fileSelection"]["name"]);
+        //shell_exec($command);
+        //exit;
     ?>
     <form action="index.html">
-      <input type="submit" value="Back"/>
+      <input type="submit" value="Back">
     </form>
     <div class="header">
-      &nbsp;</br></br></br>      
+      &nbsp;<br><br><br>
     </div>
   </body>
 </html>
