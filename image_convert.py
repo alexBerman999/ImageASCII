d0706a1607fdd7397f5c3d8fb914428fbf737aeb
Jason Carrete <jasoncarrete5@gmail.com>
2018-05-19 19:04:13 -0400

    made ascii_array_to_html func more pythonic

--- image_convert.py
+++ image_convert.py
@@ -13,7 +13,7 @@ def img_to_ascii(img_path):
     :return: A list representing the ascii art image as ascii
     """
     # Read image
-    img = Image.open(img_path, "r")
+    img = Image.open(img_path, 'r')
     # Reduce image size
     scale_size = (300, int(img.size[1] * (150/img.size[0])))
     img = img.resize(scale_size, Image.ANTIALIAS)
@@ -45,17 +45,14 @@ def ascii_array_to_html(ascii_list):
 
     :return: html representation of ascii image
     """
-    html = "<div id=\"asciiArt\" style = \"font-size: 0.3%;\">\n"
-    for i in range(len(ascii_list)):
-        for j in range(len(ascii_list[0])):
-            html += ascii_list[i][j]
-        html += "<br>\n"
-    html += "</div>"
+    html = '<div id="asciiArt" style="font-size: 0.3%;">\n'
+    html += '<br>\n'.join(''.join(row) for row in ascii_list)
+    html += '\n</div>\n'
     return html
 
 
 # Get location of image
 loc = sys.argv[1]
 # Convert image to ascii_list art
-ascii_list = img_to_ascii(loc)
-print(ascii_array_to_html(ascii_list))
+ascii_img = img_to_ascii(loc)
+print(ascii_array_to_html(ascii_img))
